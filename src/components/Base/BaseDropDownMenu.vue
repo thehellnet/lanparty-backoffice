5b857d95aacf1fffd7f640c89f0b4913443f4bee
Gavino Isoni <gavino.isoni@flosslab.com>
2020-01-24 00:12:30 +0100

    dropdwonmenu and logout

--- src/components/Base/BaseDropDownMenu.vue
+++ src/components/Base/BaseDropDownMenu.vue
@@ -1,15 +1,11 @@
 <template>
     <div class="relative">
-        <button
-            @click="isOpen = !isOpen"
-            class="relative z-10 block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white"
-        >
-            <img
-                class="h-full w-full object-cover"
-                src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
-                alt="Your avatar"
-            />
-        </button>
+        <div v-if="buttonProp.type === 'icon'">
+            <base-icon-button :icon="buttonProp.icon" @click="isOpen = !isOpen"></base-icon-button>
+        </div>
+        <div v-else>
+            <base-button :text="buttonProp.text"></base-button>
+        </div>
         <button
             v-if="isOpen"
             @click="isOpen = false"
@@ -17,27 +13,39 @@
             class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"
         ></button>
         <div v-if="isOpen" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
-            <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Account settings</a>
-            <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Support</a>
-            <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sign out</a>
+            <a
+                v-for="menuItem in menuItems"
+                :key="menuItem.title"
+                @click="menuItem.action()"
+                href="#"
+                class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
+                >{{ menuItem.title }}</a
+            >
         </div>
     </div>
 </template>
 
-<script>
-    import {Prop, Vue} from 'vue-property-decorator'
+<script lang="ts">
+import { Prop, Vue } from 'vue-property-decorator'
 import Component from 'vue-class-component'
+import BaseButton from '@/components/Base/BaseButton.vue'
+import BaseIconButton from '@/components/Base/BaseIconButton.vue'
 
 @Component({
     name: 'BaseDropDownMenu',
+    components: { BaseIconButton, BaseButton },
 })
 export default class BaseDropDownMenu extends Vue {
-    @Prop({type:Array, default: []}) menuList
-
-    private isOpen: boolean = false
+    @Prop({ type: Array, default: [] }) menuItems
+    @Prop({ type: Boolean, default: false }) isOpen
+    @Prop({ type: Object, default: { type: 'icon', icon: 'bars' } }) buttonProp: {
+        type: string
+        text?: string
+        icon?: string
+    }
 
     created() {
-        const handleEscape = (e) => {
+        const handleEscape = e => {
             if (e.key === 'Esc' || e.key === 'Escape') {
                 this.isOpen = false
             }
@@ -47,7 +55,6 @@ export default class BaseDropDownMenu extends Vue {
             document.removeEventListener('keydown', handleEscape)
         })
     }
-
 }
 </script>
 
--- src/router/layouts/TheNavBar.vue
+++ src/router/layouts/TheNavBar.vue
@@ -24,13 +24,19 @@
             >
                 {{ nav.title }}
             </router-link>
-<!--            <a-->
-<!--                v-if="!this.isAuthenticated"-->
-<!--                href="/login"-->
-<!--                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-primary hover:bg-white mt-4 lg:mt-0"-->
-<!--                >Login</a-->
-<!--            >-->
-            <BaseIconButton  v-if="this.isAuthenticated" icon="user"></BaseIconButton>
+            <!--            <a-->
+            <!--                v-if="!this.isAuthenticated"-->
+            <!--                href="/login"-->
+            <!--                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-primary hover:bg-white mt-4 lg:mt-0"-->
+            <!--                >Login</a-->
+            <!--            >-->
+            <!--            <BaseIconButton  v-if="this.isAuthenticated" icon="user"></BaseIconButton>-->
+            <base-drop-down-menu
+                v-if="this.isAuthenticated"
+                :is-open="false"
+                :button-prop="{ type: 'icon', icon: 'user' }"
+                :menu-items="userMenuItems"
+            ></base-drop-down-menu>
         </div>
     </nav>
 </template>
@@ -41,12 +47,14 @@ import { Vue } from 'vue-property-decorator'
 import BaseIconButton from '@/components/Base/BaseIconButton.vue'
 
 import { namespace } from 'vuex-class'
+import BaseDropDownMenu from '@/components/Base/BaseDropDownMenu.vue'
+import logger from '@/services/app-logger/app-logger.service'
 
 const authModule = namespace('AuthModule')
 
 @Component({
     name: 'TheNavBar',
-    components: { BaseIconButton },
+    components: { BaseDropDownMenu, BaseIconButton },
 })
 export default class TheNavBar extends Vue {
     navLinks = [
@@ -56,11 +64,30 @@ export default class TheNavBar extends Vue {
     ]
     showMenu = false
 
+    userMenuItems = [
+        {
+            title: 'LogOut',
+            action: this.signOut,
+        },
+    ]
     @authModule.Getter
     private isAuthenticated: () => boolean
 
+    @authModule.Action
+    private doLogout: () => Promise<boolean>
+
     toggleMenu() {
         this.showMenu = !this.showMenu
+    }
+
+    signOut() {
+        this.doLogout()
+            .then(() => {
+                this.$router. replace('/login',)
+            })
+            .catch(error => {
+                logger.debug(error)
+            })
     }
 }
 </script>
